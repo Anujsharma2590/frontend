531d17eaf667b70790075ef392f417f108c488a7
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-05-10 15:37:29 +0530

    chore: Update npm dependencies and add lazy loading for images

--- app/components/Transactions.js
+++ app/components/Transactions.js
@@ -1,20 +1,63 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import * as React from "react";
+import { View, useWindowDimensions, StyleSheet } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import List from "./ui/List";
 
-const Transactions = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Transactions</Text>
-    </View>
-  );
-};
+const AllRoute = () => <List />;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+const DayRoute = () => <List />;
+
+const WeekRoute = () => <List />;
+
+const MonthRoute = () => <List />;
+const YearRoute = () => <List />;
+
+const renderScene = SceneMap({
+  first: AllRoute,
+  second: DayRoute,
+  third: WeekRoute,
+  fourth: MonthRoute,
+  fifth: YearRoute,
 });
 
-export default Transactions;
+export default function Transactions() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+
+  const [routes] = React.useState([
+    { key: "first", title: "All" },
+    { key: "second", title: "Day" },
+    { key: "third", title: "Week" },
+    { key: "fourth", title: "Month" },
+    { key: "fifth", title: "Year" },
+  ]);
+
+  const tabBarStyle = {
+    backgroundColor: "#fff", // Background color of the tab bar
+    elevation: 0, // Android elevation for tab bar
+    shadowOpacity: 0, // iOS shadow opacity for tab bar
+    borderBottomWidth: 1, // Border bottom width
+    borderBottomColor: "#ccc", // Border bottom color
+    marginBottom: 10, // Margin top for tab bar
+  };
+
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: "#ff4081" }}
+      style={tabBarStyle}
+      labelStyle={{ color: "#333" }}
+    />
+  );
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+      renderTabBar={renderTabBar}
+    />
+  );
+}
