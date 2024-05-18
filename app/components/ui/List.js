729764db105d0121f08ebbd356204aa92f56fb13
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-05-18 17:23:20 +0530

    created list

--- app/components/ui/List.js
+++ app/components/ui/List.js
@@ -1,5 +1,36 @@
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import { renderDate, getIcon } from "../../utils/methods";
+
+const List = ({ data }) => {
+  const renderItem = ({ item }) => {
+    const transactionTypeStyle =
+      item.transactionType === "expense"
+        ? styles.expenseText
+        : styles.incomeText;
+
+    return (
+      <View style={styles.itemContainer}>
+        <View style={styles.row}>
+          <View style={styles.iconContainer}>
+            {getIcon(item.transactionType, item.heading)}
+          </View>
+          <View>
+            <Text style={[styles.item, styles.heading]}>{item.heading}</Text>
+            <Text style={styles.date}>{renderDate(item.date)}</Text>
+          </View>
+        </View>
+        <View>
+          <Text style={[styles.item, transactionTypeStyle]}>
+            {item.transactionType === "expense" ? "-" : "+"}${item.amount}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  return <FlatList data={data} renderItem={renderItem} />;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -9,7 +40,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     marginHorizontal: 10,
     marginBottom: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 15,
     borderRadius: 15,
     shadowColor: "#000",
     shadowRadius: 2,
@@ -22,8 +53,8 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
   item: {
-    fontSize: 22,
-    fontWeight: "bold",
+    fontSize: 20,
+    fontWeight: "500",
   },
   expenseText: {
     color: "#FF6347", // Red color for expenses
@@ -33,61 +64,25 @@ const styles = StyleSheet.create({
   },
   date: {
     color: "#808080",
+    fontSize: 12,
   },
   row: {
-    flexDirection: "column",
-    alignItems: "flex-start",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 14,
     justifyContent: "center",
+  },
+  heading: {
+    fontWeight: "bold",
+  },
+  iconContainer: {
+    backgroundColor: "#eee",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    width: 45,
+    height: 45,
   },
 });
 
-const List = ({ data }) => {
-  const renderDate = (dateString) => {
-    const date = new Date(dateString);
-    const day = date.getDate();
-    const month = date.toLocaleString("default", { month: "long" });
-    const year = date.getFullYear();
-    const formattedDate = `${day}${getOrdinalSuffix(day)} ${month} ${year}`;
-    return formattedDate;
-  };
-
-  const getOrdinalSuffix = (number) => {
-    if (number === 1 || number === 21 || number === 31) return "st";
-    if (number === 2 || number === 22) return "nd";
-    if (number === 3 || number === 23) return "rd";
-    return "th";
-  };
-  const renderItem = ({ item }) => {
-    const transactionTypeStyle =
-      item.transactionType === "expense"
-        ? styles.expenseText
-        : styles.incomeText;
-
-    return (
-      <View style={styles.itemContainer}>
-        <View style={styles.row}>
-          <Text style={styles.item}>{item.heading}</Text>
-          <Text style={styles.date}>{renderDate(item.date)}</Text>
-        </View>
-        <View>
-          <Text style={[styles.item, transactionTypeStyle]}>
-            ₹{item.amount}
-          </Text>
-        </View>
-      </View>
-    );
-  };
-
-  return (
-    <FlatList
-      data={data}
-      renderItem={renderItem}
-      // keyExtractor={(item) => item.id.toString()}
-    />
-  );
-};
-
 export default List;
-
-
-
