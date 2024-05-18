742596cb8afc1538c44b6e2442aa8da8ab0a5428
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-05-18 13:44:50 +0530

    make another folder for constants

---
+++ app/components/constants/index.js
@@ -0,0 +1,47 @@
+export const expenseDropdownData = [
+    {
+      label: "Utilities",
+      value: "utils",
+    },
+  
+    {
+      label: "Electronics",
+      value: "electronics",
+    },
+  
+    {
+      label: "Dining Out",
+      value: "dine",
+    },
+  
+    {
+      label: "Breakfast Supplies",
+      value: "breakfast",
+    },
+    {
+      label: "Groceries",
+      value: "grocery",
+    },
+  ];
+  
+  export const incomeDropdownData = [
+    {
+      label: "Bonus",
+      value: "bonus",
+    },
+  
+    {
+      label: "Part-time Job",
+      value: "parttime",
+    },
+  
+    {
+      label: "Freelance",
+      value: "freelance",
+    },
+  
+    {
+      label: "online sales",
+      value: "sales",
+    },
+  ];
