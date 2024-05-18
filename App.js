e31a6915051714139643a1e8d359288d4faa6443
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-05-18 12:33:48 +0530

    remove warning for textinput.icon

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./app/MainNavigator";
 import LoginProvider from "./app/context/LoginProvider";
 
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs(true);
+
 export default function App() {
   return (
     <LoginProvider>
