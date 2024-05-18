dc0cf43c89760c474ab35c652d0a19324418e956
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-05-18 15:02:33 +0530

    add theme for react-paper

--- App.js
+++ App.js
@@ -4,18 +4,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./app/MainNavigator";
 import LoginProvider from "./app/context/LoginProvider";
 
-import { LogBox } from 'react-native';
-import { theme } from './theme';
+import { LogBox } from "react-native";
+import { theme } from "./theme";
+import { PaperProvider } from "react-native-paper";
 
 LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
     <LoginProvider>
-      <NavigationContainer 
-       theme={theme}
-      >
-        <MainNavigator />
+      <NavigationContainer theme={theme}>
+        <PaperProvider theme={theme}>
+          <MainNavigator />
+        </PaperProvider>
       </NavigationContainer>
     </LoginProvider>
   );
--- app/components/TransactionInput.js
+++ app/components/TransactionInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button, Text, Alert } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { TextInput } from "react-native-paper";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -12,7 +12,11 @@ import { formatDate } from "../utils/methods";
 const TransactionInput = (props) => {
   const value = props.type === "income" ? "bonus" : "utils";
 
-  const [values, setValues] = useState({ heading: value, amount: "", date: new Date() });
+  const [values, setValues] = useState({
+    heading: value,
+    amount: "",
+    date: new Date(),
+  });
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [date, setDate] = useState(new Date());
 
@@ -35,7 +39,7 @@ const TransactionInput = (props) => {
     try {
       const formattedDate = formatDate(values.date);
       const transactionValues = { ...values, date: formattedDate };
-  
+
       if (props.type === "income") {
         await submitIncome(transactionValues);
       } else {
@@ -60,16 +64,16 @@ const TransactionInput = (props) => {
 
       if (response) {
         Alert.alert(
-          'Success!',
+          "Success!",
           `${response.data.message}`,
           [
             {
-              text: 'OK',
+              text: "OK",
             },
           ],
-          { cancelable: false },
+          { cancelable: false }
         );
-        setValues({ heading: value, amount: "", date: new Date() }); 
+        setValues({ heading: value, amount: "", date: new Date() });
       } else {
         console.error("Error fetching transactions:", response.statusText);
       }
@@ -87,21 +91,21 @@ const TransactionInput = (props) => {
       const response = await client.post("/expense", values, {
         headers: {
           Authorization: `${token}`, // Add the authorization token
-        }
+        },
       });
 
       if (response) {
         Alert.alert(
-          'Success!',
+          "Success!",
           `${response.data.message}`,
           [
             {
-              text: 'OK',
+              text: "OK",
             },
           ],
-          { cancelable: false },
+          { cancelable: false }
         );
-        setValues({ heading: value, amount: "", date: new Date() }); 
+        setValues({ heading: value, amount: "", date: new Date() });
       } else {
         console.error("Error fetching transactions:", response.statusText);
       }
@@ -110,47 +114,65 @@ const TransactionInput = (props) => {
     }
   };
 
-
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Picker
-          selectedValue={values.heading}
-          onValueChange={(itemValue) => handleChange("heading", itemValue)}
-          style={styles.picker}
-        >
-          {props.dropdownData.map((item, index) => (
-            <Picker.Item
-              key={index}
-              label={item.label}
-              value={item.value}
-            />
-          ))}
-        </Picker>
-      </View>
-      <TextInput
-        mode="outlined"
-        placeholder="Amount"
-        style={styles.input}
-        keyboardType="numeric"
-        onChangeText={(text) => handleChange("amount", text)}
-        value={values.amount}
-      />
-      <View style={styles.datePickerContainer}>
-        <Button onPress={toggleDatePicker} title="Select Date" />
-        {showDatePicker && (
-          <DateTimePicker
-            value={date}
-            mode="date"
-            display="calendar"
-            onChange={onChangeDate}
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Picker
+            selectedValue={values.heading}
+            onValueChange={(itemValue) => handleChange("heading", itemValue)}
+            style={styles.picker}
+            
+          >
+            {props.dropdownData.map((item, index) => (
+              <Picker.Item key={index} label={item.label} value={item.value} />
+            ))}
+          </Picker>
+        </View>
+
+        <TextInput
+          mode="outlined"
+          placeholder="Amount"
+          label="Amount"
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={(text) => handleChange("amount", text)}
+          value={values.amount}
+          left={<TextInput.Icon icon="currency-usd" />}
+        />
+
+        <TextInput
+          mode="outlined"
+          placeholder="Date"
+          label="Date"
+          style={styles.input}
+          value={values.date.toLocaleDateString()}
+          left={<TextInput.Icon icon="calendar" />}
+        />
+
+        <View style={styles.datePickerContainer}>
+          <CustomButton
+            onPress={toggleDatePicker}
+            title="Select Date"
+            disabled={false}
+            icon={"calendar"}
           />
-        )}
+          {showDatePicker && (
+            <DateTimePicker
+              value={date}
+              mode="date"
+              display="calendar"
+              onChange={onChangeDate}
+            />
+          )}
+        </View>
+        <CustomButton
+          onPress={submitTransaction}
+          title="ADD"
+          icon="plus"
+          disabled={!values.amount}
+        />
       </View>
-      <Text style={styles.dateText}>
-        Date: {values.date.toLocaleDateString()}
-      </Text>
-      <CustomButton onPress={submitTransaction} title="ADD" icon = "plus" disabled={!values.amount}/>
     </View>
   );
 };
@@ -185,6 +207,7 @@ const styles = StyleSheet.create({
     color: "red",
     marginBottom: 10,
   },
+  formContainer: {},
 });
 
 export default TransactionInput;
